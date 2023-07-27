6fcf97e56e0c6d6a60d329956c0871d071b8d496
Jordy756 <83486124+Jordy756@users.noreply.github.com>
2023-07-26 22:52:20 -0600

    Se modificaron todas las secciones. Se anadio una librería JS para el carrusel de las imagenes de los proyectos Se hicieron las validaciones en el JS

---
+++ js/validations.js
@@ -0,0 +1,37 @@
+(() => {
+    new Splide(".splide").mount();
+})();
+
+(() => {
+    const inputs = document.querySelectorAll(".input, .textarea");
+    const typeOfErrors = ["valueMissing", "typeMismatch"];
+    const errorMessages = {
+        name: {
+            valueMissing: "Este campo no puede ir vacío",
+        },
+        email: {
+            valueMissing: "Este campo no puede ir vacío",
+            typeMismatch: "El formato del correo no es válido",
+        },
+        message: {
+            valueMissing: "Este campo no puede ir vacío",
+        },
+    };
+
+    inputs.forEach(input => {
+        input.addEventListener("blur", input => validate(input.target));
+    });
+
+    function validate(input) {
+        const span = input.parentElement.querySelector(".input-error");
+        input.validity.valid ? (span.innerHTML = "") : (span.innerHTML = showMessage(input));
+    }
+
+    function showMessage(input) {
+        let message = "";
+        typeOfErrors.forEach(error => {
+            if (input.validity[error]) message = errorMessages[input.id][error];
+        });
+        return message;
+    }
+})();
